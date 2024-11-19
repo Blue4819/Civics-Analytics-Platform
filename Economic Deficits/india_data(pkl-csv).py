7baa44c1ee9fb5d27e0b3dbff657fdc0a13b58c7
Tanya Singh <98892932+TSinghh@users.noreply.github.com>
2024-11-19 15:17:38 +0530

    Add files via upload

---
+++ Economic Deficits/india_data(pkl-csv).py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Path to the input .pkl file
+input_file = r"C:\Users\Tanya\OneDrive\Desktop\Capstone Project Codes\india_data.pkl"
+
+# Load the pickle file into a DataFrame
+df = pd.read_pickle(input_file)
+
+# Path to the output .csv file
+output_file = r"C:\Users\Tanya\OneDrive\Desktop\Capstone Project Codes\india_data.csv"
+
+# Save the DataFrame as a CSV file
+df.to_csv(output_file, index=False)
+
+print(f"Data has been successfully converted to CSV and saved as '{output_file}'.")
