89c3763e16db9e72291c75e77c2a5fc9bab7d8bc
Prachi Bharti <prachi.bharti21@st.niituniversity.in>
2024-11-24 15:07:12 +0530

    Dockerized frontend and backend and created basic framework for Flask application

--- frontend/app.py
+++ frontend/app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/nationalsentiments')
+def nationalsentiments():
+    return render_template('nationalsentiments.html')
+
+@app.route('/nationaldisasters')
+def nationaldisasters():
+    return render_template('nationaldisasters.html')
+
+@app.route('/economicdeficits')
+def economicdeficits():
+    return render_template('economicdeficits.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
