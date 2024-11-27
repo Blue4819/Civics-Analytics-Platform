1d4fda7c7e93ee2ed3b05253dde201543533c265
Prachi Bharti <prachi.bharti21@st.niituniversity.in>
2024-11-27 16:07:25 +0530

    National Disasters page

--- frontend/app.py
+++ frontend/app.py
@@ -14,12 +14,20 @@ def nationalsentiments():
 @app.route('/nationaldisasters')
 def nationaldisasters():
     # Fetch data from the backend
-    response = requests.post('http://backend:8000/get-data/national_disasters')
+    response = requests.post('http://backend:8000/get-data/national_disasters_count')
     if response.status_code == 200:
-        disaster_data = response.json()
+        disaster_data_count = response.json()
     else:
-        disaster_data = []
-    return render_template('nationaldisasters.html', disaster_data=disaster_data)
+        disaster_data_count = []
+
+    response2 = requests.post('http://backend:8000/get-data/covid_data')
+    if response2.status_code == 200:
+        covid_data = response2.json()
+        min_date = min(item['Date'] for item in covid_data) if covid_data else None
+        max_date = max(item['Date'] for item in covid_data) if covid_data else None
+    else:
+        covid_data = []
+    return render_template('nationaldisasters.html', disaster_data=disaster_data_count, covid_data = covid_data, min_date = min_date, max_date=max_date)
 
 @app.route('/economicdeficits')
 def economicdeficits():
